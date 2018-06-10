810133cda2b498e7bf0f95e14bad80c062faa1db
Alexander Karrer <alex.kay.rs@gmail.com>
2018-06-10 17:36:27 +0200

    Added Reading of Rules

--- Getraenkeunfall/HomeViewController.swift
+++ Getraenkeunfall/HomeViewController.swift
@@ -16,6 +16,13 @@ class HomeViewController: UIViewController {
     var rulesGeneral: Dictionary<String, String> = [:]
     var onePlayerGame: Dictionary<String, String> = [:]
     var twoPlayerGame: Dictionary<String, String> = [:]
+    var threePlayerGame: Dictionary<String, String> = [:]
+    var onePlayerRuleGameRule: Dictionary<String, String> = [:]
+    var onePlayerRuleGameResolve: Dictionary<String, String> = [:]
+    var twoPlayerRuleGameRule: Dictionary<String, String> = [:]
+    var twoPlayerRuleGameResolve: Dictionary<String, String> = [:]
+    var threePlayerRuleGameRule: Dictionary<String, String> = [:]
+    var threePlayerRuleGameResolve: Dictionary<String, String> = [:]
     
     
     override func viewDidLoad() {
@@ -36,8 +43,70 @@ class HomeViewController: UIViewController {
         data!.enumerateLines { line, _ in lines.append(line)}
         
         for lin in lines {
-            print(lin)
+            if lin.starts(with: "#") != true {
+                switch lin.first {
+                case "G":
+                    var splittedRule = lin.split(separator: ";")
+                    if !String(splittedRule[1]).starts(with: "#") {
+                        rulesGeneral[String(splittedRule[0])] = String(splittedRule[1])
+                    }
+                case "1":
+                    switch lin[lin.index(lin.startIndex, offsetBy: 1)] {
+                    case "P":
+                        var splittedRule = lin.split(separator: ";")
+                        if !String(splittedRule[1]).starts(with: "#") {
+                            onePlayerGame[String(splittedRule[0])] = String(splittedRule[1])
+                        }
+                    case "R":
+                        var splittedRule = lin.split(separator: ";")
+                        if !String(splittedRule[1]).starts(with: "#") {
+                            onePlayerRuleGameRule[String(splittedRule[0])] = String(splittedRule[1])
+                            onePlayerRuleGameResolve[String(splittedRule[0])] = String(splittedRule[2])
+                        }
+                    default:
+                        print("Rule \(lin) could not be assigned to a Category. Make sure there is no Spelling Error in the ID." )
+                    }
+                case "2":
+                    switch lin[lin.index(lin.startIndex, offsetBy: 1)] {
+                    case "P":
+                        var splittedRule = lin.split(separator: ";")
+                        if !String(splittedRule[1]).starts(with: "#") {
+                            twoPlayerGame[String(splittedRule[0])] = String(splittedRule[1])
+                        }
+                    case "R":
+                        var splittedRule = lin.split(separator: ";")
+                        if !String(splittedRule[1]).starts(with: "#") {
+                            twoPlayerRuleGameRule[String(splittedRule[0])] = String(splittedRule[1])
+                            twoPlayerRuleGameResolve[String(splittedRule[0])] = String(splittedRule[2])
+                        }
+                    default:
+                        print("Rule \(lin) could not be assigned to a Category. Make sure there is no Spelling Error in the ID." )
+                    }
+                case "3":
+                    switch lin[lin.index(lin.startIndex, offsetBy: 1)] {
+                    case "P":
+                        var splittedRule = lin.split(separator: ";")
+                        if !String(splittedRule[1]).starts(with: "#") {
+                            threePlayerGame[String(splittedRule[0])] = String(splittedRule[1])
+                        }
+                    case "G":
+                        var splittedRule = lin.split(separator: ";")
+                        if !String(splittedRule[1]).starts(with: "#") {
+                            threePlayerRuleGameRule[String(splittedRule[0])] = String(splittedRule[1])
+                            threePlayerRuleGameResolve[String(splittedRule[0])] = String(splittedRule[2])
+                        }
+                    default:
+                        print("Rule \(lin) could not be assigned to a Category. Make sure there is no Spelling Error in the ID." )
+                    }
+                default:
+                    print("Rule \(lin) could not be assigned to a Category. Make sure there is no Spelling Error in the ID." )
+                }
+            }
         }
+        print("Testing Rules General")
+        print(onePlayerGame)
+        
+        rulesHaveBeenRead = true
     }
     
     override func didReceiveMemoryWarning() {
