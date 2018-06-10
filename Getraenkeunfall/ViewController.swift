4069d0e2b6aee6f8c567f3404ba8e6d11f987edb
Alexander Karrer <alexanderkarrer@alexanders-macbook-pro-3.local>
2018-06-10 15:06:20 +0200

    Initial Commit

---
+++ Getraenkeunfall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Getraenkeunfall
+//
+//  Created by Alexander Karrer on 10.06.18.
+//  Copyright © 2018 Alexander Karrer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
